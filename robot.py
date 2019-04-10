c134d786160a7c9283dfe19e94fc83b7ab673b1c
Eric Shapiro <eshapiro42@gmail.com>
2019-04-09 22:24:34 -0700

    Calibration now tunes Kp first and then Ki

--- robot.py
+++ robot.py
@@ -134,6 +134,7 @@ class Controller():
         Kp (float): Multiplier for the proportional component of the PID.
         Ki (float): Multiplier for the integral component of the PID.
         on (bool): Whether the control loop is on.
+        mean_square_error (float): The total mean square error over the entire path.
     '''
     def __init__(self, sensor, movement, delay, Kp, Ki):
         self.sensor = sensor
@@ -144,9 +145,10 @@ class Controller():
         self.on = False
         self.total_iterations = 0
         self.sum_errors_squared = 0
-        self.rms_error = 0
+        self.mean_square_error = 0
 
     def toggle(self):
+        '''Change the state of the controller'''
         if self.on:
             self.on = False
         else:
@@ -164,75 +166,117 @@ class Controller():
                     integral += error * self.delay
                     output = (error * self.Kp) + (integral * self.Ki)
                     last_error = error
-                    self.movement.control = -output
                     self.total_iterations += 1
                     self.sum_errors_squared += error**2
+                    self.movement.control = -output
                 else:
                     self.movement.control = 0
                 start_time = time.time()
             time.sleep(self.delay / 100)
         if self.total_iterations > 0:
-            self.rms_error = np.sqrt(self.sum_errors_squared / self.total_iterations)
+            self.mean_square_error = self.sum_errors_squared / self.total_iterations
 
 
 class Calibration():
     def __init__(self):
-    # def __init__(self, num, movement_error, sensor_error):
         # Read the config file
         with open('config.json', 'rb') as config_file:
             config = json.load(config_file)
         # Compute the config file checksum
         self.config_checksum = hashlib.sha256(open('config.json', 'rb').read()).hexdigest()
+        # Get values from config
         self.num = config['num']
         self.movement_error = config['movement_error']
         self.sensor_error = config['sensor_error']
-        self.gradient_h = .1
+        self.freq = config['freq']
+        # Gradient descent hyperparameters
+        self.cost_samples = 10
+        self.delta_h = .05
         self.learning_rate = .1
-        self.precision = .01
-        self.max_it = 50
-
-        # self.Kp_range = np.linspace(0, 1, num=10)
-        # self.Ki_range = np.linspace(0, 1, num=10)
-        # self.lowest_rms_error = None
-        # self.best_Kp = None
-        # self.best_Ki = None
+        self.precision = .05
+        self.max_it = 20
+        self.max_step = 1
 
     def cost(self, Kp, Ki):
-        robot = Robot(self.num, self.movement_error, self.sensor_error, self.num, Kp, Ki)
-        robot.controller.toggle()
-        robot.start(interactive=False)
-        return robot.controller.rms_error
+        '''Estimate the cost function for given values of Kp and Ki using <cost_samples> samples'''
+        cost_sum = 0
+        for sample in range(self.cost_samples):
+            robot = Robot(self.num, self.movement_error, self.sensor_error, self.freq, Kp, Ki)
+            robot.controller.toggle()
+            robot.start(interactive=False)
+            cost = robot.controller.mean_square_error
+            cost_sum += cost
+        avg_cost = cost_sum / self.cost_samples
+        return avg_cost
 
-    def gradient(self, Kp, Ki):
-        cost1_Kp = self.cost(Kp - self.gradient_h, Ki)
-        cost2_Kp = self.cost(Kp + self.gradient_h, Ki)
-        gradient_Kp = cost2_Kp - cost1_Kp / (2 * self.gradient_h)
-        cost1_Ki = self.cost(Kp, Ki - self.gradient_h)
-        cost2_Ki = self.cost(Kp, Ki + self.gradient_h)
-        gradient_Ki = cost2_Ki - cost1_Ki / (2 * self.gradient_h)
-        return gradient_Kp, gradient_Ki
+    def gradient_Kp(self, Kp, Ki):
+        '''Estimate Kp component of cost function's gradient at the point (Kp, Ki)'''
+        cost1_Kp = self.cost(Kp - self.delta_h, Ki)
+        cost2_Kp = self.cost(Kp + self.delta_h, Ki)
+        gradient_Kp = cost2_Kp - cost1_Kp / (2 * self.delta_h)
+        return gradient_Kp
 
-    def gradient_descent(self):
-        Kp = .5
-        Ki = .5
-        step_size_Kp = 1
-        step_size_Ki = 1
+    def gradient_Ki(self, Kp, Ki):
+        '''Estimate Ki component of cost function's gradient at the point (Kp, Ki)'''
+        cost1_Ki = self.cost(Kp, Ki - self.delta_h)
+        cost2_Ki = self.cost(Kp, Ki + self.delta_h)
+        gradient_Ki = cost2_Ki - cost1_Ki / (2 * self.delta_h)
+        return gradient_Ki
+
+    def gradient_descent_Kp(self):
+        '''Tune Kp using gradient descent and Ki=0'''
+        print('Tuning Kp...')
+        Kp = 1
+        Kp_step = 1
         it = 0
-        while (step_size_Kp > self.precision or step_size_Ki > self.precision) and it < self.max_it:
+        while abs(Kp_step) > self.precision and it < self.max_it:
             Kp_prev = Kp
-            Ki_prev = Ki
-            gradient_Kp, gradient_Ki = self.gradient(Kp, Ki)
-            Kp = Kp - self.learning_rate * gradient_Kp
-            Ki = Ki - self.learning_rate * gradient_Ki
-            step_size_Kp = abs(Kp - Kp_prev)
-            step_size_Ki = abs(Ki - Ki_prev)
-            print('it={}, Kp={}, Ki={}, step_size_Kp={}, step_size_Ki={}'.format(it, Kp, Ki, step_size_Kp, step_size_Ki))
+            gradient_Kp = self.gradient_Kp(Kp, Ki=0)
+            Kp_step = self.learning_rate * gradient_Kp
+            # step magnitudes should not exceed max_step
+            if abs(Kp_step) > self.max_step:
+                Kp_step = Kp_step / abs(Kp_step) * self.max_step
+            # recalculate Kp
+            Kp = Kp - Kp_step
+            # Kp cannot be less than zero
+            if Kp < 0:
+                Kp = 0
+            print('it={}, Kp={}, delta_Kp={}'.format(it, Kp, -Kp_step))
             it += 1
+        return Kp
+        
+    def gradient_descent_Ki(self, Kp):
+        '''Tune Ki using gradient descent and fixed Kp'''
+        print('Tuning Ki...')
+        Ki = 1
+        Ki_step = 1
+        it = 0
+        while abs(Ki_step) > self.precision and it < self.max_it:
+            Ko_prev = Ki
+            gradient_Ki = self.gradient_Ki(Kp, Ki)
+            Ki_step = self.learning_rate * gradient_Ki
+            # step magnitudes should not exceed max_step
+            if abs(Ki_step) > self.max_step:
+                Ki_step = Ki_step / abs(Ki_step) * self.max_step
+            # recalculate Ki
+            Ki = Ki - Ki_step
+            # Ki cannot be less than zero
+            if Ki < 0:
+                Ki = 0            
+            print('it={}, Ki={}, delta_Ki={}'.format(it, Ki, -Ki_step))
+            it += 1
+        return Ki
+
+    def tune(self):
+        '''Tune Kp first and then Ki using gradient descent'''
+        Kp = self.gradient_descent_Kp()
+        Ki = self.gradient_descent_Ki(Kp)
         return Kp, Ki
 
     def calibrate(self):
+        '''Perform the calibration'''
         # Tune Kp and Ki
-        Kp, Ki = self.gradient_descent()
+        Kp, Ki = self.tune()
         # Create the calibration dictionary
         cal = {
             'config_checksum': self.config_checksum,
@@ -242,32 +286,6 @@ class Calibration():
         # Save the calibration data
         with open('cal.json', 'w') as cal_file:
             json.dump(cal, cal_file)
-
-    # def calibrate(self):
-    #     # Tune Kp and Ki
-    #     # TODO This currently sweeps out a huge grid. Switch to gradient descent if there's time.
-    #     it = 0
-    #     for Kp in self.Kp_range:
-    #         for Ki in self.Ki_range:
-    #             print('{}%'.format(100 * it / (len(self.Kp_range) * len(self.Ki_range))), end='')
-    #             robot = Robot(self.num, self.movement_error, self.sensor_error, self.num, Kp, Ki)
-    #             robot.controller.toggle()
-    #             robot.start(interactive=False)
-    #             if self.lowest_rms_error is None or robot.controller.rms_error < self.lowest_rms_error:
-    #                 self.lowest_rms_error = robot.controller.rms_error
-    #                 self.best_Kp = Kp
-    #                 self.best_Ki = Ki
-    #             print(' --- Kp={}, Ki={}'.format(self.best_Kp, self.best_Ki))
-    #             it += 1
-    #     # Create the calibration dictionary
-    #     cal = {
-    #         'config_checksum': self.config_checksum,
-    #         'Kp': self.best_Kp,
-    #         'Ki': self.best_Ki,
-    #     }
-    #     # Save the calibration data
-    #     with open('cal.json', 'w') as cal_file:
-    #         json.dump(cal, cal_file)
 
 
 class Robot():
